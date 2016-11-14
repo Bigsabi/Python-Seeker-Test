71ef76b9a5d94c1774961e333205b591d2f86f84
Bigsabi <az999897@hotmail.com>
2016-11-14 22:34:38 +0800

    获得好多妹子！

--- Seeker_Main.py
+++ Seeker_Main.py
@@ -13,10 +13,30 @@ def get_limit(url=url_index):
     pattern = re.compile(r'<span class="current-comment-page">\[(\d+)\]</span>')
     limit = pattern.findall(html)
     return limit[0]
+def get_imgs(url):
+    html = get_html(url)
+    pattern = re.compile(r"[a-zA-z]+://[^\s]*\.jpg")
+    imgs = pattern.findall(html)
+    return imgs
+def download_imgs(imgs):
+    img_set = set(imgs)
+    imgs = list(img_set)
+    for i in range(len(imgs)):
+        img_url = imgs[i]
+        #img_data = urlopen(img_url).read() 
+        try:
+            img_data = urlopen(img_url).read()
+        except:
+            continue
+        with open(str(i)+'.jpg','wb') as f:
+            f.write(img_data)
 '''
 html = get_html(url_index)
 print(html[:50])
 '''
-
-limit = get_limit()
-print(limit)
+#[a-zA-z]+://[^\s]*\.jpg
+#limit = get_limit()
+#print(limit)
+imgs =get_imgs(url_index) 
+#print(get_imgs(url_index))
+download_imgs(imgs)
